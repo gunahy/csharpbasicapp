4ac45a175b23c76de1ebb9e3de13958e60e037b4
gunahy <freshdv@mail.ru>
2024-07-22 21:52:53 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        if (args is null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+
+        DaysOfWeek MyFavoriteDay;
+
+        MyFavoriteDay = DaysOfWeek.Friday;
+
+        Console.WriteLine(MyFavoriteDay);
+
+        Console.WriteLine(Semaphore.Green);
+    }
+}
+
+enum DaysOfWeek : byte
+{
+    Tuesday,
+    Monday,
+    Wednesday,
+    Friday
+}
+
+enum Semaphore : int
+{
+    Red = 100,
+    Yellow = 200,
+    Green = 300,
+}
